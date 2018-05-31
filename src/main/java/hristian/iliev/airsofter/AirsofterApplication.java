2d7d2fecf1088038af28e09007633be9d948ebda
Hristian Iliev <hristian00i@abv.bg>
2018-05-31 20:45:10 +0300

    Configuring the application

---
+++ src/main/java/hristian/iliev/airsofter/AirsofterApplication.java
@@ -0,0 +1,11 @@
+package hristian.iliev.airsofter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AirsofterApplication {
+  public static void main(String[] args) {
+    SpringApplication.run(AirsofterApplication.class, args);
+  }
+}
---
+++ src/test/java/hristian/iliev/airsofter/AirsofterApplicationTests.java
@@ -0,0 +1,16 @@
+package hristian.iliev.airsofter;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class AirsofterApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
